0c1f25e8e6e8254c04f499a5eaf0262b213871c5
Thiago Miranda <thiago.miranda@brf.com>
2022-08-10 21:15:14 -0300

    finishing temperature converter

---
+++ temperature_converter/target/debug/deps/temperature_converter-5d8a12bec02f99b7.d
@@ -0,0 +1,5 @@
+c:\Users\624590\OneDrive - BRF S.A\Disco P\Desktop\Pessoal\rust-studies\temperature_converter\target\debug\deps\temperature_converter-5d8a12bec02f99b7.rmeta: src\main.rs
+
+c:\Users\624590\OneDrive - BRF S.A\Disco P\Desktop\Pessoal\rust-studies\temperature_converter\target\debug\deps\temperature_converter-5d8a12bec02f99b7.d: src\main.rs
+
+src\main.rs:
