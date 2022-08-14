1369c4c63dba38b3cde86cd12da3cf57c4d031cc
Thiago Miranda <thiago.miranda@brf.com>
2022-08-14 00:18:01 -0300

    build code

---
+++ fibonnaci/target/debug/deps/fibonnaci-f7f000752f00288a.d
@@ -0,0 +1,5 @@
+c:\Users\624590\OneDrive - BRF S.A\Disco P\Desktop\Pessoal\rust-studies\fibonnaci\target\debug\deps\fibonnaci-f7f000752f00288a.rmeta: src\main.rs
+
+c:\Users\624590\OneDrive - BRF S.A\Disco P\Desktop\Pessoal\rust-studies\fibonnaci\target\debug\deps\fibonnaci-f7f000752f00288a.d: src\main.rs
+
+src\main.rs:
---
+++ fibonnaci/target/debug/fibonnaci.d
@@ -0,0 +1,1 @@
+C:\Users\624590\OneDrive\ -\ BRF\ S.A\Disco\ P\Desktop\Pessoal\rust-studies\fibonnaci\target\debug\fibonnaci.exe: C:\Users\624590\OneDrive\ -\ BRF\ S.A\Disco\ P\Desktop\Pessoal\rust-studies\fibonnaci\src\main.rs
